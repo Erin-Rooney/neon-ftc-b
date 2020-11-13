ed3fde1ced0157042e4fb71ca3ad003e407609f7
Erin-Rooney <61806923+Erin-Rooney@users.noreply.github.com>
2020-11-13 13:25:53 -0800

    Create AK-ER-NEONprecip.R

---
+++ code/AK-ER-NEONprecip.R
@@ -0,0 +1,8 @@
+#Erin C Rooney
+#NEON precip data - combining files
+#Import - permafrost only
+#Updated 11-13-2020
+
+PRIPRE = read.csv("processed/PRIPRE_2017_2020_5cores.csv")
+SECPRE = read.csv("processed/SECPRE_2017_2020_5cores.csv")
+
